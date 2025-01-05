7018bc94a4b532786acd193089ef274b361a8426
Kabil Jbeli <kabiljbeli@Kabils-MacBook-Air.local>
2025-01-05 19:02:40 +0100

    fix

--- src/app/models/userlist-response.ts
+++ src/app/models/userlist-response.ts
@@ -1,5 +1,13 @@
 export interface UserlistResponse {
-  content:any[];
+  content:User[];
   totalElements:number;
 
+}
+
+
+export interface User{
+  firstName:string;
+  lastName:string;
+  email:string;
+  phone:string;
 }
